28c98bf43316fb247a9f764f11a9bfec7a066937
Thomas Tang <tphz@yahoo.com>
2023-07-20 17:08:36 +0800

    修改了计算圆平方的方式

--- Day01-15/code/Day02/circle.py
+++ Day01-15/code/Day02/circle.py
@@ -9,6 +9,6 @@ import math
 
 radius = float(input('请输入圆的半径: '))
 perimeter = 2 * math.pi * radius
-area = math.pi * radius * radius
+area = math.pi * radius ** 2
 print('周长: %.2f' % perimeter)
 print('面积: %.2f' % area)
